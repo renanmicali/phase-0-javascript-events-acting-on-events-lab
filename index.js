28897a36d4d3394045366680215679fabb2772c6
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-09-28 12:08:49 +1000

    adding event listeners

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4"
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 7}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360) {
+        dodger.style.left = `${left + 7}px`
+    }
+}
+
+function moveDodgerUp() {
+    const upNumbers = dodger.style.bottom.replace("px","");
+    const up = parseInt(upNumbers, 10)
+
+    if (up < 380) {
+        dodger.style.up = `${up + 7}px`
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    } else if (e.key === "ArrowUp"){
+        moveDodgerUp();
+    }
+  });  
+
+
+
+// document.addEventListener("keydown", function(event){
+//     if (event.key === "ArrowRight"){
+//         moveDodgerRight();
+//     }
+// })
